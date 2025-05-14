f9226945c988141c19e3f7f7156dea5c07b665de
jusoaresg <julianosgreg@gmail.com>
2025-05-14 19:23:41 -0300

    feat: add easier way to change tracking status and create episode

--- internal/db/model/episode.go
+++ internal/db/model/episode.go
@@ -12,6 +12,16 @@ const (
 	StatusDownloaded TrackingStatus = "downloaded"
 )
 
+type trackingStatusEnum struct{}
+
+var Tracking = trackingStatusEnum{}
+
+func (trackingStatusEnum) Wanted() TrackingStatus     { return StatusWanted }
+func (trackingStatusEnum) Missing() TrackingStatus    { return StatusMissing }
+func (trackingStatusEnum) Skipped() TrackingStatus    { return StatusSkipped }
+func (trackingStatusEnum) Snatched() TrackingStatus   { return StatusSnatched }
+func (trackingStatusEnum) Downloaded() TrackingStatus { return StatusDownloaded }
+
 type Episode struct {
 	ID       uint `gorm:"primaryKey"`
 	ShowId   int  `gorm:"index"`
@@ -22,9 +32,31 @@ type Episode struct {
 	Season   int
 	AirStamp string
 
-	Installed bool
-	FilePath  *string
-	Tracking  TrackingStatus `gorm:"type:text;default:'wanted'"`
+	//Installed   bool
+	FilePath    *string
+	Tracking    TrackingStatus `gorm:"type:text;default:'wanted'"`
+	TorrentHash string
+}
+
+func (e *Episode) Create(
+	ShowId int,
+	Name string,
+	Summary string,
+	Type string,
+	Number int,
+	Season int,
+	AirStamp string,
+) *Episode {
+	return &Episode{
+		ShowId:   ShowId,
+		Name:     Name,
+		Summary:  Summary,
+		Type:     Type,
+		Number:   Number,
+		Season:   Season,
+		AirStamp: AirStamp,
+		//Installed: false,
+	}
 }
 
 func (e *Episode) AirDate() string {
@@ -35,4 +67,14 @@ func (e *Episode) AirDate() string {
 func (e *Episode) AirTime() string {
 	t, _ := time.Parse(time.RFC3339, e.AirStamp)
 	return t.Format("15:04")
+}
+
+func (e *Episode) HasAired() (bool, error) {
+	airTime, err := time.Parse(time.RFC3339, e.AirStamp)
+	if err != nil {
+		return false, err
+	}
+
+	// If the airTime is before or equal the time now, then it has been aired
+	return !airTime.After(time.Now()), nil
 }
