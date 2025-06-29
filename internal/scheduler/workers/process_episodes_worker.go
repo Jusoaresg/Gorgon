ad96781747cf5010d0cb992b2c19f64318f83fed
jusoaresg <julianosgreg@gmail.com>
2025-06-29 02:29:01 -0300

    feat: checkAllConnections to check if all services are connected before processing episodes

--- internal/scheduler/workers/process_episodes_worker.go
+++ internal/scheduler/workers/process_episodes_worker.go
@@ -1,32 +1,44 @@
 package workers
 
 import (
+	"log/slog"
+
 	"github.com/jusoaresg/gorgon/config"
 	prowlarr "github.com/jusoaresg/gorgon/external/prowlarr/service"
 	qbittorrent "github.com/jusoaresg/gorgon/external/qbittorrent/service"
 	"github.com/jusoaresg/gorgon/internal/episode/model"
 	"github.com/jusoaresg/gorgon/internal/scheduler/jobs"
-	"log/slog"
+	"github.com/jusoaresg/gorgon/pkg/services"
 )
 
 func processEpisodesWorker(episodesChan <-chan model.Episode) {
-	logger := config.GetLogger()
-	prowlarrService := prowlarr.NewProwlarrSearchService(logger)
+	logger := config.GetLogger().WithGroup("worker").With("name", "processEpisodeWorker")
+	prowlarrService, err := prowlarr.NewProwlarrSearchService(logger)
+	if err != nil {
+		logger.Error("Error initializing prowlarr service", slog.String("error", err.Error()))
+		return
+	}
 	qbittorrentService, err := qbittorrent.NewQBittorrentService(logger)
 	if err != nil {
 		logger.Error("Error initializing qbittorrent service", slog.String("error", err.Error()))
 		return
 	}
 
+	errs := services.CheckAllConnections(prowlarrService, qbittorrentService)
+	if len(errs) > 0 {
+		logger.Error("Failed to connect to one or more services", slog.Any("errors", err))
+		return
+	}
+
+	episodeLogger := logger.WithGroup("episode")
 	for episode := range episodesChan {
 		err := jobs.ProcessSingleEpisode(&episode, *prowlarrService, *qbittorrentService)
 		if err != nil {
-			logger.Error(
-				"Error processing episode",
+			episodeLogger.Error(
+				"Error processing single episode",
 				slog.Int("episodeID", int(episode.ID)),
 				slog.Int64("showID", episode.ShowID),
 				slog.String("episodeName", episode.Name),
-				slog.String("worker", "processEpisodeWorker"),
 				slog.String("error", err.Error()))
 		}
 	}
