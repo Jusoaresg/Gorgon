e93cc3fe2e32047f3aa81f57e9a59cdff371462e
jusoaresg <julianosgreg@gmail.com>
2025-06-29 00:45:12 -0300

    feat: better logging with file creationg on assets/logs

--- config/logger.go
+++ config/logger.go
@@ -1,12 +1,33 @@
 package config
 
 import (
+	"fmt"
+	"io"
+	"log"
 	"log/slog"
 	"os"
+	"path/filepath"
+	"time"
 )
 
 func NewLogger() *slog.Logger {
-	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+	err := os.MkdirAll("assets/logs", os.ModePerm)
+	if err != nil {
+		log.Fatalf("Failed to create log directory: %v", err)
+	}
+
+	today := time.Now().Format("2006-01-02")
+	filename := fmt.Sprintf("gorgon-%s.log", today)
+	path := filepath.Join("assets", "logs", filename)
+
+	logFile, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+	if err != nil {
+		log.Fatalf("Failed to open log file: %v", err)
+	}
+
+	multiWriter := io.MultiWriter(os.Stdout, logFile)
+
+	handler := slog.NewJSONHandler(multiWriter, &slog.HandlerOptions{
 		Level: slog.LevelDebug,
 	})
 
