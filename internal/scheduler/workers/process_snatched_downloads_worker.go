c80088e093cdf1f67269083aa345a5334da5ff79
jusoaresg <julianosgreg@gmail.com>
2025-06-29 02:29:56 -0300

    feat: check all connections before processing snatched downloads

--- internal/scheduler/workers/process_snatched_downloads_worker.go
+++ internal/scheduler/workers/process_snatched_downloads_worker.go
@@ -1,30 +1,38 @@
 package workers
 
 import (
+	"log/slog"
+
 	"github.com/jusoaresg/gorgon/config"
 	"github.com/jusoaresg/gorgon/external/qbittorrent/service"
 	"github.com/jusoaresg/gorgon/internal/episode/model"
 	"github.com/jusoaresg/gorgon/internal/scheduler/jobs"
-	"log/slog"
+	"github.com/jusoaresg/gorgon/pkg/services"
 )
 
 func processSnatchedDownloadsWorker(episodesChan <-chan model.Episode) {
-	logger := config.GetLogger()
+	logger := config.GetLogger().WithGroup("worker").With("name", "processSnatchedDownloadsWorker")
 	qbittorrentService, err := service.NewQBittorrentService(logger)
 	if err != nil {
 		logger.Error("Error initializing qbittorrent service", slog.String("error", err.Error()))
 		return
 	}
 
+	errs := services.CheckAllConnections(qbittorrentService)
+	if len(errs) > 0 {
+		logger.Error("Failed to connect to one or more services", slog.Any("errors", errs))
+		return
+	}
+
+	episodeLogger := logger.WithGroup("episode").With("source", "snatched")
 	for episode := range episodesChan {
 		err := jobs.ProcessSingleSnatchedDownload(&episode, *qbittorrentService)
 		if err != nil {
-			logger.Error(
-				"Error processing episode",
+			episodeLogger.Error(
+				"Error processing snatched episode downloaded",
 				slog.Int("episodeID", int(episode.ID)),
 				slog.Int64("showID", episode.ShowID),
 				slog.String("episodeName", episode.Name),
-				slog.String("worker", "processSnatchedDownloadsWorker"),
 				slog.String("error", err.Error()))
 		}
 	}
