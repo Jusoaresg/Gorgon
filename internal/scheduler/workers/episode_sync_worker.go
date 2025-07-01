cc2f8114231077ec8eec280895d0b96ac22dc873
jusoaresg <julianosgreg@gmail.com>
2025-07-01 01:52:17 -0300

    feat: checking airstamp when fetching episodes from database feat: better logging for episode sync worker

--- internal/scheduler/workers/episode_sync_worker.go
+++ internal/scheduler/workers/episode_sync_worker.go
@@ -1,22 +1,28 @@
 package workers
 
 import (
-	"github.com/jusoaresg/gorgon/config"
-	"github.com/jusoaresg/gorgon/internal/episode/model"
+	"log/slog"
 	"sync"
 	"time"
+
+	"github.com/jusoaresg/gorgon/config"
+	"github.com/jusoaresg/gorgon/internal/episode/model"
 
 	"github.com/jmoiron/sqlx"
 )
 
 func StartEpisodeSyncWorker(workerCount int) {
+	logger := config.GetLogger().WithGroup("worker").With("name", "episodeSync")
 	episodeChan := make(chan model.Episode, 50)
 	var wg sync.WaitGroup
 
-	for range workerCount {
+	logger.Info("Starting episode sync workers", slog.Int("count", workerCount))
+
+	for i := 0; i < workerCount; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
+			logger.Info("Worker started")
 			processEpisodesWorker(episodeChan)
 		}()
 	}
@@ -26,30 +32,46 @@ func StartEpisodeSyncWorker(workerCount int) {
 
 	for {
 		<-ticker.C
+		logger.Info("Checking for wanted episodes")
 
 		episodes := fetchWantedEpisodes()
 		if len(episodes) == 0 {
+			logger.Info("No episodes found with status 'wanted' or 'missing'")
 			continue
 		}
 
 		for _, ep := range episodes {
+			logger.Info("Queuing episode for processing",
+				slog.Int("episodeID", int(ep.ID)),
+				slog.Int64("showID", ep.ShowID),
+				slog.String("name", ep.Name),
+			)
 			episodeChan <- ep
 		}
 	}
 }
 
 func fetchWantedEpisodes() []model.Episode {
+	logger := config.GetLogger().WithGroup("worker").With("name", "episodeSync")
 	var episodes []model.Episode
 	db := config.GetSQLite()
 
-	query, args, err := sqlx.In("SELECT * FROM episodes WHERE tracking IN (?) LIMIT 20", []string{"wanted", "missing"})
+	query, args, err := sqlx.In(
+		`SELECT * FROM episodes WHERE tracking IN (?) 
+			AND airstamp <= ? 
+		LIMIT 20`,
+		[]string{"wanted", "missing"},
+		time.Now().UTC(),
+	)
 	if err != nil {
+		logger.Error("Failed to build SQL query with sqlx.In", slog.String("error", err.Error()))
 		return nil
 	}
 	query = db.Rebind(query)
 
 	err = db.Select(&episodes, query, args...)
 	if err != nil {
+		logger.Error("Failed to execute SELECT query", slog.String("error", err.Error()))
 		return nil
 	}
 
