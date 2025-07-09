f7ae60792c3f92fe20ff120250e1af2b226d3fd0
jusoaresg <julianosgreg@gmail.com>
2025-07-09 04:47:23 -0300

    feat: add rate limiter to prowlarr search service

--- external/prowlarr/service/prowlarr_search_service.go
+++ external/prowlarr/service/prowlarr_search_service.go
@@ -1,18 +1,23 @@
 package service
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
+	"net/url"
+	"time"
+
 	"github.com/jusoaresg/gorgon/config"
 	"github.com/jusoaresg/gorgon/external/prowlarr/schema"
 	"github.com/jusoaresg/gorgon/pkg/services"
-	"log/slog"
-	"net/url"
+	"golang.org/x/time/rate"
 )
 
 type ProwlarrSearchService struct {
 	ApiKey     string
 	APIService services.APIService
 	Logger     *slog.Logger
+	limiter    *rate.Limiter
 }
 
 func NewProwlarrSearchService(logger *slog.Logger) (*ProwlarrSearchService, error) {
@@ -27,11 +32,19 @@ func NewProwlarrSearchService(logger *slog.Logger) (*ProwlarrSearchService, erro
 		ApiKey:     configFile.ProwlarrApiKey,
 		Logger:     logger,
 		APIService: *services.NewAPIService(fmt.Sprintf("%s:%s", prowlarrHost, prowlarrPort), logger),
+		limiter:    rate.NewLimiter(rate.Every(2*time.Second), 1),
 	}, nil
 }
 
 func (p *ProwlarrSearchService) Name() string {
 	return "ProwlarrSearch"
+}
+
+func (p *ProwlarrSearchService) waitTicker(ctx context.Context) error {
+	if err := p.limiter.Wait(ctx); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (p *ProwlarrSearchService) CheckConnection() error {
@@ -46,16 +59,18 @@ func (p *ProwlarrSearchService) CheckConnection() error {
 }
 
 func (p *ProwlarrSearchService) Search(request *schema.SearchRequest, model *[]schema.SearchResponse) error {
+	if err := p.waitTicker(context.Background()); err != nil {
+		return err
+	}
 	queryEscaped := url.QueryEscape(request.Query)
 	return p.APIService.Get(fmt.Sprintf("/api/v1/search?query=%s&apikey=%s", queryEscaped, p.ApiKey), &model)
 }
 
 func (p *ProwlarrSearchService) SearchByType(request *schema.SearchByTypeRequest, model *[]schema.SearchResponse) error {
+	if err := p.waitTicker(context.Background()); err != nil {
+		return err
+	}
 	queryEscaped := url.QueryEscape(request.Query)
 	typeEscaped := url.QueryEscape(request.Type)
 	return p.APIService.Get(fmt.Sprintf("/api/v1/search?query=%s&type=%s&apikey=%s", queryEscaped, typeEscaped, p.ApiKey), &model)
 }
-
-// func (p *ProwlarrSearchService) GetIndexer(id string, model *schema.IndexerResponse) error {
-// 	return p.APIService.Get(fmt.Sprintf("/api/v1/indexer/%s?apikey=%s", id, p.ApiKey), &model)
-// }
