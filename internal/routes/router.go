75552f6fe1f64db8357408449bb35ab2366f5bfe
jusoaresg <julianosgreg@gmail.com>
2026-01-02 18:33:27 -0300

    feat: change scalar docs to rapidocs

--- internal/routes/router.go
+++ internal/routes/router.go
@@ -1,14 +1,13 @@
 package routes
 
 import (
-	"fmt"
+	_ "embed"
+	"log/slog"
+	"net/http"
+
 	"github.com/jusoaresg/gorgon/config"
 	"github.com/jusoaresg/gorgon/docs"
 	"github.com/jusoaresg/gorgon/pkg/handler"
-	"github.com/jusoaresg/gorgon/pkg/schemas"
-	"log/slog"
-
-	"github.com/MarceloPetrucio/go-scalar-api-reference"
 	"github.com/labstack/echo/v4"
 )
 
@@ -45,24 +44,11 @@ func InitializeRoutes(e *echo.Echo) {
 
 	logger.Info("Routes initialized", slog.String("basePath", basePath))
 
+	e.GET("/swagger.json", func(c echo.Context) error {
+		return c.Blob(http.StatusOK, "application/json", docs.SwaggerJSON)
+	})
+
 	v1.GET("docs", func(c echo.Context) error {
-		htmlContent, err := scalar.ApiReferenceHTML(&scalar.Options{
-			CDN:     "https://cdn.jsdelivr.net/npm/@scalar/api-reference@latest",
-			SpecURL: "./docs/swagger.yaml",
-			CustomOptions: scalar.CustomOptions{
-				PageTitle: "Gorgon",
-			},
-			DarkMode: true,
-		})
-
-		if err != nil {
-			logger.Error("Error while generating API documentation", slog.String("error", err.Error()))
-			schemas.SendError(c, 500, fmt.Sprintf("Error while getting Scalar Api Reference: %s", err.Error()))
-			return err
-		}
-
-		c.HTML(200, htmlContent)
-		logger.Info("API Documentation generated successfully", slog.String("url", "/docs"))
-		return nil
+		return c.Render(http.StatusOK, "docs.html", nil)
 	})
 }
