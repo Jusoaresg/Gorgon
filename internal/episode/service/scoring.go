d6848c288ab6c239366843fed2dc084d840066b6
jusoaresg <julianosgreg@gmail.com>
2026-04-07 22:21:33 -0300

    feat: better episode score system

--- internal/episode/service/scoring.go
+++ internal/episode/service/scoring.go
@@ -10,20 +10,23 @@ func getPreferredWordsScore(t schema.SearchResponse) int {
 	preferredWord := make(map[string]int)
 	preferredWord["multisubs"] = 50
 
-	for word, score := range preferredWord {
-		if strings.Contains(strings.ToLower(t.Filename), word) {
-			return score
+	filename := strings.ToLower(t.Filename)
+
+	score := 0
+	for word, points := range preferredWord {
+		if strings.Contains(filename, word) {
+			score += points
 		}
 	}
-	return 0
+	return score
 }
 
 func getQualityScore(t schema.SearchResponse) int {
 	possibleQuality := make(map[string]int)
-	possibleQuality["2560"] = 9
-	possibleQuality["1080"] = 10
-	possibleQuality["720"] = 8
-	possibleQuality["480"] = 1
+	possibleQuality["2560"] = 20
+	possibleQuality["1080"] = 20
+	possibleQuality["720"] = 10
+	possibleQuality["480"] = 0
 
 	for key, quality := range possibleQuality {
 		if strings.Contains(t.Filename, key) {
@@ -34,6 +37,15 @@ func getQualityScore(t schema.SearchResponse) int {
 }
 
 func ScoreEpisode(t schema.SearchResponse) int {
+	score := getPreferredWordsScore(t) + getQualityScore(t)
+
+	health := t.Seeders - t.Leechers
+	if health > 30 {
+		health = 30
+	} else if health < 0 {
+		health = 0
+	}
+
 	//TODO: Better detection for the filesize
-	return (t.Seeders - t.Leechers) + getPreferredWordsScore(t) + getQualityScore(t) //- int(t.Size)
+	return score + health
 }
