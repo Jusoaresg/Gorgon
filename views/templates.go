f1740327bb006077326a3eb1308d667274db2339
jusoaresg <julianosgreg@gmail.com>
2026-07-18 06:54:53 +0000

    feat(templates): add template functions "dict" and "airDate"

--- views/templates.go
+++ views/templates.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"strings"
 
+	"github.com/jusoaresg/gorgon/internal/episode/model"
 	episodeModel "github.com/jusoaresg/gorgon/internal/episode/model"
 	"github.com/labstack/echo/v4"
 )
@@ -61,9 +62,30 @@ func NewTemplate() *Template {
 			}
 			return int((float64(part) / float64(total)) * 100)
 		},
+		"dict": func(values ...any) (map[string]any, error) {
+			if len(values)%2 != 0 {
+				return nil, fmt.Errorf("invalid dict call")
+			}
+			dict := make(map[string]any, len(values)/2)
+			for i := 0; i < len(values); i += 2 {
+				key, ok := values[i].(string)
+				if !ok {
+					return nil, fmt.Errorf("dict keys must be strings")
+				}
+				dict[key] = values[i+1]
+			}
+			return dict, nil
+		},
+		"airDate": func(e model.Episode) string { return e.AirDate() },
 	})
 
-	tmpl, err := tmpl.ParseFS(FrontFS, "ui/*.html", "ui/components/*.html", "ui/components/settings/*.html")
+	tmpl, err := tmpl.ParseFS(
+		FrontFS,
+		"ui/*.html",
+		"ui/components/*.html",
+		"ui/components/settings/*.html",
+		"ui/components/show/*.html",
+	)
 	if err != nil {
 		panic(fmt.Errorf("error while loading embedded front %w", err))
 	}
